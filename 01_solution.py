2fb7210c38f11568cdd01cc684c9b1b81dfa7638
Aditya Sharma <147606124+AdityaSharma271004@users.noreply.github.com>
2025-09-01 22:30:04 +0530

    Add files via upload

---
+++ 01_solution.py
@@ -0,0 +1,12 @@
+# Age Group Catergerization
+
+age = int(input("enter age: "))
+
+if(age <13):
+    print("you are child.")
+elif(age >=13 and age< 19):
+    print("you are teenager.")    
+elif(age>=19 and age< 25):
+    print("you are adult.")    
+elif(age >=25 and age<=60):
+    print("you are getting old myson.")    
