1462045e1771706354121dc8113a114eb91f96d6
Aditya Sharma <147606124+AdityaSharma271004@users.noreply.github.com>
2025-09-06 15:34:35 +0530

    Add files via upload

---
+++ loops problems/06_solution.py
@@ -0,0 +1,15 @@
+# Factorial Calculator
+
+num = int(input("enter the number: "))
+ 
+factorial = 1 
+
+
+while(num>0):
+    factorial = factorial * num
+    num = num -1
+
+print(factorial)    
+
+
+    
