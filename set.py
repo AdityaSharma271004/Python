fb39116f5f300960ecfc5bbb66c1a8659b0aacfe
Aditya Sharma <147606124+AdityaSharma271004@users.noreply.github.com>
2026-02-04 15:56:50 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,26 @@
+s ={1,5,53}
+
+e=set() #dont use s = {} as it will create an empty dictionary
+
+# print(s)
+
+# methods of set
+
+# print(s,type(s))
+
+s.add(566)
+# print(s)
+s.remove(566)
+# print(s)
+
+# set union and intersection
+
+s1={1,2,3}
+s2={4,5,6,3}
+ 
+print(s1.union(s2))
+print(s1.intersection(s2))
+
+ 
+
+
