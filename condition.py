e26e461b7df045e30d06b7ef6293732b09cad981
Aditya Sharma <147606124+AdityaSharma271004@users.noreply.github.com>
2026-02-05 08:14:04 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,15 @@
+# if-elif-else
+
+age = int(input("enter your age:"))
+if(age>=18):
+    print("eligible")
+    
+elif(age<0):
+    print("why are you input wrong age...")
+elif(age==0):
+    print("why are you input age 0")
+
+
+else:
+    print("not eligible")    
+
