88bc186250723491983aa630352859d21f3fc2bf
Aditya Sharma <147606124+AdityaSharma271004@users.noreply.github.com>
2026-02-06 09:09:18 +0530

    Add files via upload

---
+++ loops problems/loop_breaks.py
@@ -0,0 +1,31 @@
+# break -> break the loop right now where it find the condition
+
+'''
+marks = [23,56,12,90,45]
+for  nums in marks:
+    
+    if(nums == 12):
+        break
+    print(nums)'''
+
+
+# continue -> it skip the value which was given in the condition and loop executed    
+
+'''
+marks = [23,56,12,90,45]
+for  nums in marks:
+    
+    if(nums == 12):
+        continue
+    print(nums)'''
+
+
+# pass -> it allows you to run another condition without ending for loop    
+
+marks = [23,56,12,90,45]
+for  nums in marks:
+    pass
+
+
+for i in range(10):
+        print(i)
