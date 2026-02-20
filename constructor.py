04ad2dd53a3340d4eb22f0ed76d32db87c13bde8
Aditya Sharma <147606124+AdityaSharma271004@users.noreply.github.com>
2026-02-20 08:05:54 +0530

    Add files via upload

---
+++ constructor.py
@@ -0,0 +1,30 @@
+class employee: #class
+    name= "aditya"          #class attribute
+    profession = "youtuber" #class attribute
+    salary = "1.5cr"        #class attribute
+    language = "python"     #class attribute
+    
+    
+    def __init__(self,name,salary,language): #dunder method, which is automatically called 
+        self.name = name
+        self.salary = salary
+        self.language = language
+        print("I am creating an object")    
+
+    def getInfo(self):
+        print(f"the language is {self.language} and salary is {self.salary}")
+    
+    @staticmethod #if you don't want to pass the self argument so you declare your method or function as @staticmethod.
+    def greet():
+        print("good morning sir..")
+
+
+
+object = employee("adi" , 1300000, "python")
+print(object.name,object.salary,object.language)
+object.language = "javascript" #instance object -> which takes the preference as  first.
+# print(object.name,object.profession,object.salary,object.language)  
+object.greet()
+object.getInfo()  
+# employee.getInfo(object) #same working as line no-13.
+
